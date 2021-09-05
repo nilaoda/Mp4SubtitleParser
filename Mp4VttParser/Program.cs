70a6a8f9f270260955637783d6bc1ef9da11ebda
nilaoda <nilaoda@live.com>
2021-09-05 23:03:25 +0800

    add files

--- Mp4VttParser/Program.cs
+++ Mp4VttParser/Program.cs
@@ -8,189 +8,239 @@ namespace Mp4VttParser
 {
     class Program
     {
+        static void Error(string msg)
+        {
+            Console.BackgroundColor = ConsoleColor.Red;
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.Write(msg);
+            Console.ResetColor();
+            Console.WriteLine();
+        }
+
         static void Main(string[] args)
         {
-            var data = File.ReadAllBytes(@"samples\init.mp4");
-            uint timescale = 0;
-            bool sawWVTT = false;
-
-            //parse init
-            new MP4Parser()
-                .Box("moov", MP4Parser.Children)
-                .Box("trak", MP4Parser.Children)
-                .Box("mdia", MP4Parser.Children)
-                .FullBox("mdhd", (box) =>
+            try
+            {
+                if (args.Length == 0)
                 {
-                    if (!(box.Version == 0 || box.Version == 1))
-                        throw new Exception("MDHD version can only be 0 or 1");
-                    timescale = MP4Parser.ParseMDHD(box.Reader, box.Version);
-                })
-                .Box("minf", MP4Parser.Children)
-                .Box("stbl", MP4Parser.Children)
-                .FullBox("stsd", MP4Parser.SampleDescription)
-                .Box("wvtt", (box) => {
+                    Error("arg missing...");
+                    Console.WriteLine("Mp4VttParse <subtitle dir> <segments search pattern>");
+                    return;
+                }
+
+                var dir = args[0];
+                if (string.IsNullOrEmpty(dir))
+                    dir = Environment.CurrentDirectory;
+                if (!Directory.Exists(dir))
+                    throw new Exception("Directory not exists");
+
+                var search = args[1];
+                if (string.IsNullOrEmpty(search))
+                    throw new Exception("Search pattern should not be empty");
+                var items = Directory.EnumerateFiles(dir, search);
+
+                if (!File.Exists($"{dir}\\init.mp4"))
+                    throw new Exception(Path.GetFullPath($"{dir}\\init.mp4") + "not exists!");
+
+                var data = File.ReadAllBytes($"{dir}\\init.mp4");
+                uint timescale = 0;
+                bool sawWVTT = false;
+
+                //parse init
+                new MP4Parser()
+                    .Box("moov", MP4Parser.Children)
+                    .Box("trak", MP4Parser.Children)
+                    .Box("mdia", MP4Parser.Children)
+                    .FullBox("mdhd", (box) =>
+                    {
+                        if (!(box.Version == 0 || box.Version == 1))
+                            throw new Exception("MDHD version can only be 0 or 1");
+                        timescale = MP4Parser.ParseMDHD(box.Reader, box.Version);
+                    })
+                    .Box("minf", MP4Parser.Children)
+                    .Box("stbl", MP4Parser.Children)
+                    .FullBox("stsd", MP4Parser.SampleDescription)
+                    .Box("wvtt", (box) => {
                     // A valid vtt init segment, though we have no actual subtitles yet.
                     sawWVTT = true;
-                })
-                .Parse(data);
+                    })
+                    .Parse(data);
 
-            if (timescale == 0)
-                throw new Exception("Missing timescale for VTT content!");
+                if (timescale == 0)
+                    throw new Exception("Missing timescale for VTT content!");
 
-            if (!sawWVTT)
-                throw new Exception("A WVTT box not found!");
+                if (!sawWVTT)
+                    throw new Exception("A WVTT box not found!");
 
-            Console.WriteLine($"timescale: {timescale}");
+                Console.WriteLine($"timescale: {timescale}");
 
-            List<Cue> cues = new List<Cue>();
+                List<Cue> cues = new List<Cue>();
 
-            int i = 0;
-            ttttttttttttt:
-            i++;
-            Console.Title = i.ToString();
-            if (i > 423)
-            {
-                foreach (var cue in cues)
+
+                foreach (var item in items)
                 {
-                    Console.WriteLine($"{ConvertTime(cue.StartTime)} --> {ConvertTime(cue.EndTime)} {(string.IsNullOrEmpty(cue.Settings) ? "position:50% line:94% align:middle" : cue.Settings)}\r\n" +
-                        $"{cue.Payload}\r\n");
-                }
-                Environment.Exit(0);
-            }
-            var dataSeg = File.ReadAllBytes(@"samples\segment-1." + i.ToString("0000") + ".m4s");
+                    if (Path.GetFileName(item) == "init.mp4")
+                        continue;
+                    var dataSeg = File.ReadAllBytes(item);
 
-            bool sawTFDT = false;
-            bool sawTRUN = false;
-            bool sawMDAT = false;
-            byte[] rawPayload = null;
-            ulong baseTime = 0;
-            ulong defaultDuration = 0;
-            List<Sample> presentations = new List<Sample>();
+                    bool sawTFDT = false;
+                    bool sawTRUN = false;
+                    bool sawMDAT = false;
+                    byte[] rawPayload = null;
+                    ulong baseTime = 0;
+                    ulong defaultDuration = 0;
+                    List<Sample> presentations = new List<Sample>();
 
 
-            //parse media
-            new MP4Parser()
-                .Box("moof", MP4Parser.Children)
-                .Box("traf", MP4Parser.Children)
-                .FullBox("tfdt", (box) =>
-                {
-                    sawTFDT = true;
-                    if (!(box.Version == 0 || box.Version == 1))
-                        throw new Exception("TFDT version can only be 0 or 1");
-                    baseTime = MP4Parser.ParseTFDT(box.Reader, box.Version);
-                })
-                .FullBox("tfhd", (box) =>
-                {
-                    if (box.Flags == 1000)
-                        throw new Exception("A TFHD box should have a valid flags value");
-                    defaultDuration = MP4Parser.ParseTFHD(box.Reader, box.Flags).DefaultSampleDuration;
-                })
-                .FullBox("trun", (box) =>
-                {
-                    sawTRUN = true;
-                    if (box.Version == 1000)
-                        throw new Exception("A TRUN box should have a valid version value");
-                    if (box.Flags == 1000)
-                        throw new Exception("A TRUN box should have a valid flags value");
-                    presentations = MP4Parser.ParseTRUN(box.Reader, box.Version, box.Flags).SampleData;
-                })
-                .Box("mdat", MP4Parser.AllData((data) =>
-                {
-                    if (sawMDAT)
-                        throw new Exception("VTT cues in mp4 with multiple MDAT are not currently supported");
-                    sawMDAT = true;
-                    rawPayload = data;
-                }))
-                .Parse(dataSeg,/* partialOkay= */ false);
-
-            if (!sawMDAT && !sawTFDT && !sawTRUN)
-            {
-                throw new Exception("A required box is missing");
-            }
-
-            var currentTime = baseTime;
-            var reader = new BinaryReader2(new MemoryStream(rawPayload));
-
-            foreach (var presentation in presentations)
-            {
-                var duration = presentation.SampleDuration == 0 ? defaultDuration : presentation.SampleDuration;
-                var startTime = presentation.SampleCompositionTimeOffset != 0 ?
-                      baseTime + presentation.SampleCompositionTimeOffset :
-                      currentTime;
-                currentTime = startTime + duration;
-                var totalSize = 0;
-                do
-                {
-                    // Read the payload size.
-                    var payloadSize = (int)reader.ReadUInt32();
-                    totalSize += payloadSize;
-
-                    // Skip the type.
-                    var payloadType = reader.ReadUInt32();
-                    var payloadName = MP4Parser.TypeToString(payloadType);
-
-                    // Read the data payload.
-                    byte[] payload = null;
-                    if (payloadName == "vttc")
-                    {
-                        if (payloadSize > 8)
+                    //parse media
+                    new MP4Parser()
+                        .Box("moof", MP4Parser.Children)
+                        .Box("traf", MP4Parser.Children)
+                        .FullBox("tfdt", (box) =>
                         {
-                            payload = reader.ReadBytes(payloadSize - 8);
-                        }
-                    }
-                    else if (payloadName == "vtte")
+                            sawTFDT = true;
+                            if (!(box.Version == 0 || box.Version == 1))
+                                throw new Exception("TFDT version can only be 0 or 1");
+                            baseTime = MP4Parser.ParseTFDT(box.Reader, box.Version);
+                        })
+                        .FullBox("tfhd", (box) =>
+                        {
+                            if (box.Flags == 1000)
+                                throw new Exception("A TFHD box should have a valid flags value");
+                            defaultDuration = MP4Parser.ParseTFHD(box.Reader, box.Flags).DefaultSampleDuration;
+                        })
+                        .FullBox("trun", (box) =>
+                        {
+                            sawTRUN = true;
+                            if (box.Version == 1000)
+                                throw new Exception("A TRUN box should have a valid version value");
+                            if (box.Flags == 1000)
+                                throw new Exception("A TRUN box should have a valid flags value");
+                            presentations = MP4Parser.ParseTRUN(box.Reader, box.Version, box.Flags).SampleData;
+                        })
+                        .Box("mdat", MP4Parser.AllData((data) =>
+                        {
+                            if (sawMDAT)
+                                throw new Exception("VTT cues in mp4 with multiple MDAT are not currently supported");
+                            sawMDAT = true;
+                            rawPayload = data;
+                        }))
+                        .Parse(dataSeg,/* partialOkay= */ false);
+
+                    if (!sawMDAT && !sawTFDT && !sawTRUN)
                     {
-                        // It's a vtte, which is a vtt cue that is empty. Ignore any data that
-                        // does exist.
-                        reader.ReadBytes(payloadSize - 8);
-                    }
-                    else
-                    {
-                        Console.WriteLine($"Unknown box {payloadName}! Skipping!");
-                        reader.ReadBytes(payloadSize - 8);
+                        throw new Exception("A required box is missing");
                     }
 
-                    if (duration != 0)
+                    var currentTime = baseTime;
+                    var reader = new BinaryReader2(new MemoryStream(rawPayload));
+
+                    foreach (var presentation in presentations)
                     {
-                        if (payload != null)
+                        var duration = presentation.SampleDuration == 0 ? defaultDuration : presentation.SampleDuration;
+                        var startTime = presentation.SampleCompositionTimeOffset != 0 ?
+                              baseTime + presentation.SampleCompositionTimeOffset :
+                              currentTime;
+                        currentTime = startTime + duration;
+                        var totalSize = 0;
+                        do
                         {
-                            if (timescale == 0)
-                                throw new Exception("Timescale should not be zero!");
-                            var cue = ParseVTTC(
-                                payload,
-                                0 + (double)startTime / timescale,
-                                0 + (double)currentTime / timescale);
-                            //Check if same subtitle has been splitted
-                            var index = cues.FindLastIndex(s => s.EndTime == cue.StartTime && s.Settings == cue.Settings && s.Payload == cue.Payload);
-                            if (index != -1)
+                            // Read the payload size.
+                            var payloadSize = (int)reader.ReadUInt32();
+                            totalSize += payloadSize;
+
+                            // Skip the type.
+                            var payloadType = reader.ReadUInt32();
+                            var payloadName = MP4Parser.TypeToString(payloadType);
+
+                            // Read the data payload.
+                            byte[] payload = null;
+                            if (payloadName == "vttc")
                             {
-                                cues[index].EndTime = cue.EndTime;
+                                if (payloadSize > 8)
+                                {
+                                    payload = reader.ReadBytes(payloadSize - 8);
+                                }
+                            }
+                            else if (payloadName == "vtte")
+                            {
+                                // It's a vtte, which is a vtt cue that is empty. Ignore any data that
+                                // does exist.
+                                reader.ReadBytes(payloadSize - 8);
                             }
                             else
                             {
-                                cues.Add(cue);
+                                Console.WriteLine($"Unknown box {payloadName}! Skipping!");
+                                reader.ReadBytes(payloadSize - 8);
                             }
+
+                            if (duration != 0)
+                            {
+                                if (payload != null)
+                                {
+                                    if (timescale == 0)
+                                        throw new Exception("Timescale should not be zero!");
+                                    var cue = ParseVTTC(
+                                        payload,
+                                        0 + (double)startTime / timescale,
+                                        0 + (double)currentTime / timescale);
+                                    //Check if same subtitle has been splitted
+                                    var index = cues.FindLastIndex(s => s.EndTime == cue.StartTime && s.Settings == cue.Settings && s.Payload == cue.Payload);
+                                    if (index != -1)
+                                    {
+                                        cues[index].EndTime = cue.EndTime;
+                                    }
+                                    else
+                                    {
+                                        cues.Add(cue);
+                                    }
+                                }
+                            }
+                            else
+                            {
+                                throw new Exception("WVTT sample duration unknown, and no default found!");
+                            }
+
+                            if (!(presentation.SampleSize == 0 || totalSize <= presentation.SampleSize))
+                            {
+                                throw new Exception("The samples do not fit evenly into the sample sizes given in the TRUN box!");
+                            }
+
+                        } while (presentation.SampleSize != 0 && (totalSize < presentation.SampleSize));
+
+                        if (reader.HasMoreData())
+                        {
+                            //throw new Exception("MDAT which contain VTT cues and non-VTT data are not currently supported!");
                         }
                     }
-                    else
-                    {
-                        throw new Exception("WVTT sample duration unknown, and no default found!");
-                    }
+                }
 
-                    if (!(presentation.SampleSize == 0 || totalSize <= presentation.SampleSize))
-                    {
-                        throw new Exception("The samples do not fit evenly into the sample sizes given in the TRUN box!");
-                    }
-
-                } while (presentation.SampleSize != 0 && (totalSize < presentation.SampleSize));
-
-                if (reader.HasMoreData())
+                if (cues.Count > 0)
                 {
-                    //throw new Exception("MDAT which contain VTT cues and non-VTT data are not currently supported!");
+                    StringBuilder sb = new StringBuilder();
+                    sb.AppendLine("WEBVTT");
+                    sb.AppendLine("");
+                    sb.AppendLine("");
+                    foreach (var cue in cues)
+                    {
+                        sb.AppendLine($"{ConvertTime(cue.StartTime)} --> {ConvertTime(cue.EndTime)} {(string.IsNullOrEmpty(cue.Settings) ? "position:50% line:94% align:middle" : cue.Settings)}");
+                        sb.AppendLine(cue.Payload);
+                        sb.AppendLine();
+                        //Console.WriteLine($"{ConvertTime(cue.StartTime)} --> {ConvertTime(cue.EndTime)} {(string.IsNullOrEmpty(cue.Settings) ? "position:50% line:94% align:middle" : cue.Settings)}\r\n" +
+                        //    $"{cue.Payload}\r\n");
+                    }
+
+                    var outName = "output";
+                    if (args.Length > 2)
+                        outName = args[2];
+                    File.WriteAllText(outName + ".vtt", sb.ToString(), new UTF8Encoding(false));
+                    Console.WriteLine("Done: " + Path.GetFullPath(outName + ".vtt"));
                 }
             }
-
-            goto ttttttttttttt;
+            catch (Exception ex)
+            {
+                Error(ex.Message);
+            }
         }
 
         private static Cue ParseVTTC(byte[] data, double startTime, double endTime)
